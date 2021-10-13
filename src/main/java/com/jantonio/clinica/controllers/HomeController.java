c9d02cf2aa59576914b31a1fb78adadf899c76e0
Jhonathan Antonio Colque <jantoniocolque@outlook.com>
2021-10-13 14:35:32 -0300

    experimentando con el home

---
+++ src/main/java/com/jantonio/clinica/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.jantonio.clinica.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HomeController {
+	@GetMapping("/")
+	public String home() {
+		return "swagger-ui";
+	}
+}
